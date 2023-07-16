5a4cae4c91ef817596750a1a200c5765c20e22ce
EthanRule <94942533+EthanRule@users.noreply.github.com>
2023-07-16 09:01:34 -0700

    Text Box

--- Strategist.lua
+++ Strategist.lua
@@ -15,11 +15,21 @@ title:SetText("My Addon Window")
 local closeButton = CreateFrame("Button", nil, frame, "UIPanelCloseButton")
 closeButton:SetPoint("TOPRIGHT", frame, "TOPRIGHT", -8, -8)
 
+local editBox = CreateFrame("EditBox", nil, frame)
+editBox:SetSize(200, 20)
+editBox:SetPoint("TOPLEFT", 16, -40)
+editBox:SetFontObject(GameFontNormal)
+editBox:SetAutoFocus(false)
+editBox:SetMultiLine(false)
+editBox:SetText("Enter text here")
+
+frame.editBox = editBox -- Store the reference to the editBox in the frame for later use
+
 frame:Show()
+
 SLASH_STRATEGIST1 = "/strategist"
 
 local function OpenStrategistWindow()
-    -- Your code to open the window goes here
     if not MyAddonFrame:IsShown() then
         MyAddonFrame:Show()
     end
