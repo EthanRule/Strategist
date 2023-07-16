fbb2eb2c8ed924df3279040b40c5ca9e20fb60c6
Moffi-bit <lorenzostiavelli6@gmail.com>
2023-07-16 17:32:15 -0400

    Saved variables stuff

--- Strategist.lua
+++ Strategist.lua
@@ -1,17 +1,86 @@
-local StrategistAce = LibStub("AceAddon-3.0"):NewAddon("Strategist", "AceConsole-3.0")
-local StrategistConsole = LibStub("AceConsole-3.0")
+local Strategist = LibStub("AceAddon-3.0"):NewAddon("Strategist", "AceConsole-3.0", "AceEvent-3.0")
+local AC = LibStub("AceConfig-3.0")
+local ACD = LibStub("AceConfigDialog-3.0")
 
+local defaults = {
+	profile = {
+		message = "Welcome Home!",
+		showOnScreen = true,
+	},
+}
 
-function StrategistAce:OnInitialize()
-    StrategistAce:Print("Hello, world!")
-    self.db = LibStub("AceDB-3.0"):New("StrategistDB")
+local options = {
+	name = "Strategist",
+	handler = Strategist,
+	type = "group",
+	args = {
+		msg = {
+			type = "input",
+			name = "Message",
+			desc = "The message to be displayed when you get home.",
+			usage = "<Your message>",
+			get = "GetMessage",
+			set = "SetMessage",
+		},
+		showOnScreen = {
+			type = "toggle",
+			name = "Show on Screen",
+			desc = "Toggles the display of the message on the screen.",
+			get = "IsShowOnScreen",
+			set = "ToggleShowOnScreen"
+		},
+	},
+}
+
+function Strategist:OnInitialize()
+	self.db = LibStub("AceDB-3.0"):New("StrategistDB", defaults, true)
+	AC:RegisterOptionsTable("Strategist_options", options)
+	self.optionsFrame = ACD:AddToBlizOptions("Strategist_options", "Strategist")
+
+	local profiles = LibStub("AceDBOptions-3.0"):GetOptionsTable(self.db)
+	AC:RegisterOptionsTable("Strategist_Profiles", profiles)
+	ACD:AddToBlizOptions("Strategist_Profiles", "Profiles", "Strategist")
+
+	self:RegisterChatCommand("strat", "SlashCommand")
+	self:RegisterChatCommand("strategist", "SlashCommand")
 end
 
-function StrategistAce:OnEnable()
-    -- Called when the addon is enabled
+function Strategist:OnEnable()
+	self:RegisterEvent("ZONE_CHANGED")
 end
 
-function StrategistAce:OnDisable()
-    -- Called when the addon is disabled
+function Strategist:ZONE_CHANGED()
+	if GetBindLocation() == GetSubZoneText() then
+		if self.db.profile.showOnScreen then
+			UIErrorsFrame:AddMessage(self.db.profile.message, 1, 1, 1)
+		else
+			self:Print(self.db.profile.message)
+		end
+	end
 end
 
+function Strategist:SlashCommand(msg)
+	if not msg or msg:trim() == "" then
+		-- https://github.com/Stanzilla/WoWUIBugs/issues/89
+		InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
+		InterfaceOptionsFrame_OpenToCategory(self.optionsFrame)
+	else
+		self:Print("hello there!")
+	end
+end
+
+function Strategist:GetMessage(info)
+	return self.db.profile.message
+end
+
+function Strategist:SetMessage(info, value)
+	self.db.profile.message = value
+end
+
+function Strategist:IsShowOnScreen(info)
+	return self.db.profile.showOnScreen
+end
+
+function Strategist:ToggleShowOnScreen(info, value)
+	self.db.profile.showOnScreen = value
+end
