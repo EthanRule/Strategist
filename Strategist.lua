a5528cc70cf5a0363c785adcaac2d852b91815db
EthanRule <94942533+EthanRule@users.noreply.github.com>
2023-07-16 09:21:19 -0700

    json stuff

--- Strategist.lua
+++ Strategist.lua
@@ -1,5 +1,5 @@
 local frame = CreateFrame("Frame", "MyAddonFrame", UIParent, "BasicFrameTemplate")
-frame:SetSize(400, 300)
+frame:SetSize(400, 200)
 frame:SetPoint("CENTER")
 frame:SetMovable(true)
 frame:EnableMouse(true)
@@ -11,6 +11,21 @@ frame:SetScript("OnDragStop", frame.StopMovingOrSizing)
 local title = frame:CreateFontString(nil, "OVERLAY", "GameFontHighlight")
 title:SetPoint("TOP", frame, "TOP", 0, -8)
 title:SetText("My Addon Window")
+
+local function SaveTextToJson(text)
+    local json = LibStub("json")
+    local data = { text = text }
+
+    -- Convert the data to a JSON string
+    local jsonString = json.encode(data)
+
+    -- Save the JSON string to a file
+    local file = io.open("path/to/file.json", "w")
+    if file then
+        file:write(jsonString)
+        file:close()
+    end
+end
 
 local editBox = CreateFrame("EditBox", nil, frame)
 editBox:SetSize(200, 80) -- Adjust the width and height as needed
@@ -24,7 +39,13 @@ editBox:SetScript("OnEnterPressed", function(self)
     self:SetTextInsets(0, 0, 0, 0) -- Reset the text insets to prevent unnecessary scrolling
     self:ScrollToEnd()             -- Scroll to the end of the text box
     self:ClearFocus()              -- Clear the focus after pressing Enter
+
+    local text = self:GetText()    -- Get the entered text
+    SaveTextToJson(text)           -- Call a function to save the text to a JSON file
 end)
+
+
+
 editBox:SetText("Enter text here")
 
 frame.editBox = editBox -- Store the reference to the editBox in the frame for later use
